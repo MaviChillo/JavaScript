9eee589a233bea1a8d09c602cbdf85814eaf30ae
Mavi Chillo <mavi.chillo@gmail.com>
2022-08-29 16:53:46 -0300

    subida desafio clase 4

--- app.js
+++ app.js
@@ -1,45 +1,58 @@
 // 
 
+function compra(){
 let articulos = prompt(`Seleccione el producto que desea comprar
 1: Remera
 2: Pantalon
 3: Campera
-4: Zapatillas`)
+4: Zapatillas`);
+
 
 switch (articulos){
     case "1":
-        alert("El precio de la remera es $1200. Haga click en Aceptar para pasar al metodo de pago")
-        console.log("Remera $1200")
+        alert("El precio de la remera es $1200.");
+        console.log("Remera $1200");
     break
     case "2": 
-        alert("El precio del pantalon es $3500. Haga click en Aceptar para pasar al metodo de pago")
-        console.log("Pantalon $3500")
+        alert("El precio del pantalon es $3500.");
+        console.log("Pantalon $3500");
     break
     case "3":
-        alert("El precio de la campera es $5700. Haga click en Aceptar para pasar al metodo de pago")
-        console.log("Campera $5700")
+        alert("El precio de la campera es $5700.");
+        console.log("Campera $5700");
     break
     case "4":
-        alert("El precio de las zapatillas es $12000. Haga click en Aceptar para pasar al metodo de pago")
-        console.log("Zapatillas $12000")
+        alert("El precio de las zapatillas es $12000.");
+        console.log("Zapatillas $12000");
     break
     default:
-        alert("No has introducido una opcion correcta")
-        console.log("No has introducido una opcion correcta")
+        alert("No has introducido una opcion correcta");
+        console.log("No has introducido una opcion correcta");
     break
 }
 
-const metodoPago = prompt(`Indique el metodo de pago`)
+if (articulos != null) {
+    const metodoPago = prompt(`Indique el metodo de pago:
+efectivo
+tarjeta
+cheque
+(por favor, en minuscula)`);
 
 if (metodoPago == "efectivo" ||
 metodoPago == "tarjeta" ||
 metodoPago == "cheque"){
-    alert(`Has pagado con ${metodoPago}`)
-    console.log(`Has pagado con ${metodoPago}`)
-    alert("Compra exitosa! Muchas gracias por comprar con nosotros!")
-    console.log("Compra exitosa!")
+    alert(`Has pagado con ${metodoPago}`);
+    console.log(`Has pagado con ${metodoPago}`);
+    alert("Compra exitosa! Muchas gracias por comprar con nosotros!");
+    console.log("Compra exitosa!");
 } else {
-    alert("No ingreso datos correctos")
-    console.log("No ingreso datos correctos")
+    alert("No ingreso datos correctos");
+    console.log("No ingreso datos correctos");
+}
+} else {
+    alert("Al no elegir una de las opciones, no podemos continuar");
 }
 
+}
+
+asa();
