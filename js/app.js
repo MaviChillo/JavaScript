ac976218ff20d88b981488ee5427e0da47d160d9
Mavi Chillo <mavi.chillo@gmail.com>
2022-10-06 14:46:21 -0300

    agregando fetch

--- js/app.js
+++ js/app.js
@@ -1,39 +1,5 @@
 // 
 
-const remeras = [
-    {id: 1, producto: "Remera Red Cross", precio: 2500},
-    {id: 2, producto: "Remera The Fighter", precio: 2500},
-    {id: 3, producto: "Remera Album Art", precio: 2500},
-    {id: 4, producto: "Remera Phantom Tomorrow", precio: 2500}
-]
-
-const buzos = [
-    {id: 1, producto: "Buzo Blessed and Cursed", precio: 5500},
-    {id: 2, producto: "Buzo Phantom Tomorrow", precio: 5500},
-    {id: 3, producto: "Buzo Scarlet Cross", precio: 5500},
-    {id: 4, producto: "Buzo Wounds", precio: 5500}
-]
-
-const discos = [
-    {id: 1, producto: "Vinilo Phantom Tomorrow", precio: 2600},
-    {id: 2, producto: "Cd Phantom Tomorrow", precio: 1200},
-    {id: 3, producto: "Cd Wretched and Divine", precio: 1000},
-    {id: 4, producto: "Vinilo Re-stitch These Wounds", precio: 2500}
-]
-
-const accesorios = [
-    {id: 1, producto: "Buttons Born Again", precio: 1000},
-    {id: 2, producto: "Patches Phantom Tomorrow", precio: 2000},
-    {id: 3, producto: "Beanie Black Veil Brides", precio: 3000},
-    {id: 4, producto: "Necklace Black Veil Brides", precio: 2000}
-]
-
-const productos = [
-    {id: 1, producto: remeras},
-    {id: 2, producto: buzos},
-    {id: 3, producto: discos},
-    {id: 4, producto: accesorios}
-]
 
 const carrito = []
 console.log(carrito)
@@ -44,7 +10,6 @@ const carritoProductos = []
 console.log(carritoProductos)
 
 
-
 function compra(){
 
     const cart = document.querySelector("#ddm");
@@ -52,7 +17,7 @@ function compra(){
 
     for (let carritoProducto of carritoProductos){ 
 
-        lista.innerHTML = carritoProducto
+    lista.innerHTML = carritoProducto
 
         cart.appendChild(lista);
 
@@ -60,15 +25,26 @@ function compra(){
 }
 
 
-
 const carritoProductosArray = JSON.parse(localStorage.getItem("Carrito"))
     let resultado;
+
 
 function prod() {
 
 // remeras
-    
-    const reme1 =document.querySelector("#reme1")
+
+    const remeras = [] //crear array vacio
+
+    fetch("data.json") //fetch(``)//pushear a un array vacio llamado remeras
+    .then((res) => res.json())
+    .then ((data) => {
+        remeras.push({id: 1, producto: "Remera Red Cross", precio: 2500},
+        {id: 2, producto: "Remera The Fighter", precio: 2500},
+        {id: 3, producto: "Remera Album Art", precio: 2500},
+        {id: 4, producto: "Remera Phantom Tomorrow", precio: 2500})
+    })
+
+const reme1 =document.querySelector("#reme1")
 
 reme1.onclick = function(){
     console.log(`${remeras[0].producto} $${remeras[0].precio}`);
@@ -87,8 +63,8 @@ reme1.onclick = function(){
     } 
 
 
-
 const reme2 =document.querySelector("#reme2")
+
 
 reme2.onclick = function(){
     console.log(`${remeras[1].producto} $${remeras[1].precio}`);
@@ -148,6 +124,17 @@ reme4.onclick = function(){
 
 
 // buzos
+
+const buzos = [] //crear array vacio
+
+fetch("data.json") //fetch(``)//pushear a un array vacio llamado remeras
+.then((res) => res.json())
+.then ((data) => {
+    buzos.push({id: 5, producto: "Buzo Blessed and Cursed", precio: 5500},
+    {id: 6, producto: "Buzo Phantom Tomorrow", precio: 5500},
+    {id: 7, producto: "Buzo Scarlet Cross", precio: 5500},
+    {id: 8, producto: "Buzo Wounds", precio: 5500})
+})
 
 const buzo1 =document.querySelector("#buzo1")
 
@@ -231,6 +218,17 @@ buzo4.onclick = function(){
 
 // discos
 
+const discos = [] //crear array vacio
+
+fetch("data.json") //fetch(``)//pushear a un array vacio llamado remeras
+.then((res) => res.json())
+.then ((data) => {
+    discos.push({id: 9, producto: "Vinilo Phantom Tomorrow", precio: 2600},
+    {id: 10, producto: "Cd Phantom Tomorrow", precio: 1200},
+    {id: 11, producto: "Cd Wretched and Divine", precio: 1000},
+    {id: 12, producto: "Vinilo Re-stitch These Wounds", precio: 2500})
+})
+
 const discos1 =document.querySelector("#discos1")
 
 discos1.onclick = function(){
@@ -311,6 +309,17 @@ discos4.onclick = function(){
 
 
 // accessorios
+
+const accesorios = [] //crear array vacio
+
+fetch("data.json") //fetch(``)//pushear a un array vacio llamado remeras
+.then((res) => res.json())
+.then ((data) => {
+    accesorios.push({id: 13, producto: "Buttons Born Again", precio: 1000},
+    {id: 14, producto: "Patches Phantom Tomorrow", precio: 2000},
+    {id: 15, producto: "Beanie Black Veil Brides", precio: 3000},
+    {id: 16, producto: "Necklace Black Veil Brides", precio: 2000})
+})
 
 const acces1 =document.querySelector("#acces1")
 
@@ -396,6 +405,8 @@ acces4.onclick = function(){
 
 
 // carrito
+
+
 
 
 const buy =document.querySelector("#buy")
